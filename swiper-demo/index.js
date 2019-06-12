d04e908b5eb95a1a5a08d213681bc8bd7e6bf172
heyang <heyanggo@gmail.com>
2019-06-12 23:16:34 +0800

    无缝轮播

---
+++ swiper-demo/index.js
@@ -0,0 +1,39 @@
+let n;
+init();
+setInterval(()=>{
+    makeLeave(getImg(n)).one('transitionend', (e)=>{
+        makeEnter($(e.currentTarget))
+    })
+    makeCurrent(getImg(n+1))
+    n += 1
+},3000)
+
+function init() {
+    n = 1;
+    $('.imgBox> img:nth-child('+ n +')').addClass('current').siblings().addClass('enter')
+}
+function x(n) {
+    if (n>4){
+        n = n % 4
+        if (n===0){
+            n = 4
+        }
+    }
+    console.log(n,'1111111111')
+    return n
+}
+function getImg(n) {
+    return $('.imgBox> img:nth-child('+ x(n) +')')
+}
+function makeCurrent($node) {
+    return $node.removeClass('enter leave').addClass('current')
+}
+function makeLeave($node) {
+    return $node.removeClass('enter current').addClass('leave')
+}
+function makeEnter($node) {
+    return $node.removeClass('leave current').addClass('enter')
+}
+
+
+
