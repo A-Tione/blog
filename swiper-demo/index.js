06674ab81d8acf50ccea54774df8a4f6fd1bbf3f
heyang <heyanggo@gmail.com>
2019-06-14 00:28:18 +0800

    无缝轮播

--- swiper-demo/index.js
+++ swiper-demo/index.js
@@ -1,41 +1,41 @@
 let n;
 init();
-//核心代码
-setInterval(()=>{
-    makeLeave(getImg(n)).one('transitionend', (e)=>{
-        makeEnter($(e.currentTarget))
-    })
-    makeCurrent(getImg(n+1))
-    n += 1
-},3000)
-//end
 
-function init() {
-    n = 1;
-    $('.imgBox> img:nth-child('+ n +')').addClass('current').siblings().addClass('enter')
+setInterval(() => {
+    afterStatus(thisImg(n)).one('transitionend', (e) => {
+        beforeStatus($(e.currentTarget))
+    })
+    centerStatus(thisImg(n + 1))
+    n++
+}, 1000)
+
+function thisImg(n) {
+    return $('.imgBox>img:nth-child(' + x(n) + ')')
 }
+
 function x(n) {
-    if (n>4){
+    if (n > 4) {
         n = n % 4
-        if (n===0){
+        if (n === 0) {
             n = 4
         }
     }
-    console.log(n,'1111111111')
     return n
 }
-function getImg(n) {
-    return $('.imgBox> img:nth-child('+ x(n) +')')
-}
-function makeCurrent($node) {
-    return $node.removeClass('enter leave').addClass('current')
-}
-function makeLeave($node) {
-    return $node.removeClass('enter current').addClass('leave')
-}
-function makeEnter($node) {
-    return $node.removeClass('leave current').addClass('enter')
+
+function init() {
+    n = 1
+    $('.imgBox>img:nth-child(' + n + ')').addClass('center').siblings().addClass('before')
 }
 
+function beforeStatus($node) {
+    return $node.removeClass('center after').addClass('before')
+}
 
+function centerStatus($node) {
+    return $node.removeClass('before after').addClass('center')
+}
 
+function afterStatus($node) {
+    return $node.removeClass('before center').addClass('after')
+}
